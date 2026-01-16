5bc485b4b4ccf81bf23062cf9360c3d1b2056c41
BJames <blakejosephjames.1@gmail.com>
2026-01-16 18:19:44 +0000

    database initial setup

--- backend/main.py
+++ backend/main.py
@@ -1,5 +1,8 @@
-from fastapi import FastAPI
-from .app.core.config import settings
+from fastapi import FastAPI, Depends
+from sqlalchemy.orm import Session
+
+from backend.app.core.config import settings
+from backend.app.core.database import get_db
 
 app = FastAPI(title="Cybersecurity Training Platform")
 
@@ -9,3 +12,8 @@ async def health():
         "status": "ok",
         "database": settings.DATABASE_URL,
     }
+
+@app.get("/db-test")
+def db_test(db: Session = Depends(get_db)):
+    return {"message": "Database connection successful"}
+
